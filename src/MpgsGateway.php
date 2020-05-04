ba65b2407f165919699f704699d185e08c716baf
fiberno <indepentdent.89@gmail.com>
2020-05-04 10:03:57 +0630

    udpate libiaries

--- src/MpgsGateway.php
+++ src/MpgsGateway.php
@@ -6,50 +6,50 @@ use GuzzleHttp\Exception\ClientException;
 
 trait MpgsGateway {
 
-	public $config;
-	
-	public function setConfig($config)
-	{
-		$this->config = $config;
-	}
+    public $config;
+    
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
 
-	public function verify($attributes)
-	{	
-		$url = "{$this->config['url']}{$this->config['merchant_id']}/order/{$attributes['order_id']}/transaction/{$attributes['transaction_id']}";
+    public function verify($attributes)
+    {   
+        $url = "{$this->config['url']}{$this->config['merchant_id']}/order/{$attributes['order_id']}/transaction/{$attributes['transaction_id']}";
         $method = 'PUT';
         
         // $data = [
-	       //  'apiOperation' => 'VERIFY',
-	       //  'order' => [
-		      //   'currency' => 'MMK',
-	       //  ],
-	       //  'session' => [
-	       //  	'id' =>$attributes['session_id'],
-        // 	],
+           //  'apiOperation' => 'VERIFY',
+           //  'order' => [
+              //   'currency' => 'MMK',
+           //  ],
+           //  'session' => [
+           //   'id' =>$attributes['session_id'],
+        //  ],
         // ];
 
         //$verify = $this->request_api($url, $method, $data);
         
   //       if ($verify->result !== 'SUCCESS') {
-		// 	return [
-		// 		'success' => false, 
-		// 		'message' => 'Your card issuer bank has declined. Please contact your bank for support.',
-		// 		'error_message' => [$verify->error->cause => [$verify->error->explanation]]
-		// 	];
-		// }
+        //  return [
+        //      'success' => false, 
+        //      'message' => 'Your card issuer bank has declined. Please contact your bank for support.',
+        //      'error_message' => [$verify->error->cause => [$verify->error->explanation]]
+        //  ];
+        // }
 
-		$result = $this->getToken($attributes['session_id']);
+        $result = $this->getToken($attributes['session_id']);
 
-		if ($result) {
-			return ['success' => true, 'data' => $result];
-		}
+        if ($result) {
+            return ['success' => true, 'data' => $result];
+        }
 
-		return $result;
-	}
+        return $result;
+    }
 
-	public function getToken($sessionId)
-	{
-		$url = "{$this->config['url']}{$this->config['merchant_id']}/token";
+    public function getToken($sessionId)
+    {
+        $url = "{$this->config['url']}{$this->config['merchant_id']}/token";
         $method = 'POST';
         $data = [
             'session' => [
@@ -63,100 +63,106 @@ trait MpgsGateway {
         $response = $this->request_api($url, $method, $data);
        
         if ($response->result === 'SUCCESS' && $response->status === 'VALID') return $response;
-	}
+    }
 
-	public function delete($token)
-	{
-		$url = "{$this->config['url']}{$this->config['merchant_id']}/token/{$token}";
+    public function delete($token)
+    {
+        $url = "{$this->config['url']}{$this->config['merchant_id']}/token/{$token}";
         $method = 'DELETE';
         $response = $this->request_api($url, $method);
 
         $result['success'] = true;
         if ($response->result !== 'SUCCESS'){
-        	$result['success'] = false;
-        	$result['message'] = 'Your card can`t delete!';
-        	$result['error_message'] = [$verify->error->cause => [$verify->error->explanation]];
+            $result['success'] = false;
+            $result['message'] = 'Your card can`t delete!';
+            $result['error_message'] = [$verify->error->cause => [$verify->error->explanation]];
         } 
 
         return $result;
-	}
+    }
 
-	public function authorize($info,$token)
-	{
-		$url = "{$this->config['url']}{$this->config['merchant_id']}/order/{$info['order_id']}/transaction/{$info['transaction_id']}";
+    public function authorize($info,$token)
+    {
+        $url = "{$this->config['url']}{$this->config['merchant_id']}/order/{$info['order_id']}/transaction/{$info['transaction_id']}";
 
         $method = 'PUT';
         $data = [
             'apiOperation' => 'AUTHORIZE',
             'order' => [
                 'currency' => 'MMK',
-                'amount' => 1000
+                'amount' => $info['amount']
             ],
             'sourceOfFunds' => [
                 'token' => $token,
                 'provided' => [
-                	'card' => [ 
-                		'storedOnFile' => 'TO_BE_STORED'
-                	]
+                    'card' => [ 
+                        'storedOnFile' => 'STORED'
+                    ]
                 ]
             ],
             'transaction' => [
-            	'source' => 'INTERNET'
+                'source' => 'MERCHANT'
             ],
             'agreement' => [
-            	'type' => 'RECURRING',
-            	'id' => $info['order_id']
+                'type' => 'RECURRING',
+                'id' => $info['agreement_id']
             ]
         ];
-        return true;
-        //return $this->request_api($url, $method, $data);
-	}
+       
+        return $this->request_api($url, $method, $data);
 
-	public function agreement($info,$token)
-	{
-		$url = "{$this->config['url']}{$this->config['merchant_id']}/order/{$info['order_id']}/transaction/{$info['transaction_id']}";
+    }
+
+    public function agreement($info,$token)
+    {
+        $url = "{$this->config['url']}{$this->config['merchant_id']}/order/{$info['order_id']}/transaction/{$info['transaction_id']}";
 
         $method = 'PUT';
         $data = [
             'apiOperation' => 'AUTHORIZE',
             'order' => [
                 'currency' => 'MMK',
+                'amount' => $info['amount']
             ],
             'sourceOfFunds' => [
                 'token' => $token,
                 'provided' => [
-                	'card' => [ 
-                		'storedOnFile' => 'TO_BE_STORED'
-                	]
+                    'card' => [ 
+                        'storedOnFile' => 'TO_BE_STORED'
+                    ]
                 ]
             ],
             'transaction' => [
-            	'source' => 'INTERNET'
+                'source' => 'INTERNET'
             ],
+            'agreement' => [
+                'type' => 'RECURRING',
+                'id' => $info['agreement_id']
+            ]
         ];
       
         return $this->request_api($url, $method, $data);
-	}
+    }
 
-	private function request_api($url,$method,$data=[])
-	{
-		$data = json_encode($data);
-		$client = new Client;   
-		$header = [
-			'Authorization' => 'Basic ' . base64_encode($this->config['basic_auth']),
-			'Content-Type' => 'Application/json;charset=UTF-8',
-        	'Content-Length' => strlen($data),
-		];
+    private function request_api($url,$method,$data=[])
+    {
+        $data = json_encode($data);
+        $client = new Client;   
+        $header = [
+            'Authorization' => 'Basic ' . base64_encode($this->config['basic_auth']),
+            'Content-Type' => 'Application/json;charset=UTF-8',
+            'Content-Length' => strlen($data),
+        ];
 
         try {
-			if ($method == "GET") {
-				$response = $client->get($url);
-			}else{
-				$response = $client->request($method,$url,['body' => $data,'headers'=>$header]);  
-			}
-			return json_decode( $response->getBody()->getContents());
-		} catch (ClientException $e) {
-			return json_decode( $e->getResponse()->getBody()->getContents());
-		}  
-	}
+            if ($method == "GET") {
+                $response = $client->get($url);
+            }else{
+                $response = $client->request($method,$url,['body' => $data,'headers'=>$header]);  
+            }
+            return json_decode( $response->getBody()->getContents());
+        } catch (ClientException $e) {
+            return json_decode( $e->getResponse()->getBody()->getContents());
+        }  
+    }
 }
